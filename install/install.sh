9dadd0cefd11a3d22d56e374b3c4744b81c7eed8
Ryan Bartley <ryanbartley@gmail.com>
2016-10-03 18:09:23 -0400

    Script building on mac, removed older libs

--- install/install.sh
+++ install/install.sh
@@ -2,6 +2,8 @@
 
 # Much of the osx and ios build path based on https://gist.github.com/foozmeat/5154962
 
+cd ..
+
 lib_path=""
 target=""
 ios_sdk_version=""
@@ -32,13 +34,37 @@ fi
 
 build()
 {
-	echo "Building openssl for ${lower_case}"
 	config=$1
 	config_path=$2
-	echo ./Configure ${target} --${config} --openssldir="${lib_path}${config_path}"
-	#make
-	#make install
-	#make clean
+	# we should be starting in the blocks absolute path
+	block_absolute_path=`pwd`
+	final_library_path=${block_absolute_path}/${lib_path}${config_path}
+	final_include_path=${block_absolute_path}/lib/include
+	temp_library_path=${block_absolute_path}/tmp/openssl
+	
+	echo "Building openssl for ${lower_case}, temp_path: ${temp_library_path}, final_path: ${final_library_path}"
+	
+	rm -rf ${final_library_path}
+	rm -rf ${temp_library_path}
+	rm -rf ${final_include_path}
+	mkdir -p ${final_library_path}
+	mkdir -p ${temp_library_path}
+	mkdir -p ${final_include_path}
+
+	cd openssl
+	
+	./Configure ${target} --${config} --prefix=${temp_library_path}
+	
+	make -j 6
+	make install_sw
+	make clean
+	
+	cp -r ${temp_library_path}/include/ ${final_include_path}
+	cp ${temp_library_path}/lib/*.a ${final_library_path}
+
+	rm -rf ${block_absolute_path}/tmp
+	
+	cd ..
 }
 
 buildIos()
@@ -56,7 +82,7 @@ buildIos()
 
 	echo "Building openssl for ${platform} ${ios_sdk_version} arm64"
 
-	./Configure ${target} --${config} --openssldir="${lib_path}${config_path}"
+	./Configure ${target} --${config} --openssldir="/tmp/${lib_path}${config_path}"
 
 	sed -id "s!^CFLAG=!CFLAG=isysroot ${CROSS_TOP}/SDKs/${CROSS_SDK} -miphoneos-version-min=${SDK_VERSION} !" "Makefile"
 
@@ -65,15 +91,9 @@ buildIos()
 	make clean
 }
 
-cd ../openssl
-
 for i in 0 1 
 do
-	if [ "${lower_case}" != "ios" ]; then
-		build ${config_settings[i]} ${config_paths[i]}
-	else
-		buildIos ${config_settings[i]} ${config_paths[i]}
-	fi	
+	build ${config_settings[i]} ${config_paths[i]}
 done
 
 
