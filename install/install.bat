c1e865ec0958d5897e32ee8854ca3c21f34df80d
Ryan Bartley <ryanbartley@gmail.com>
2016-11-03 17:18:03 -0400

    Updated install script to work similarly to the others, also added batch script from Marcus, need to test and updated cinderblock.xml

---
+++ install/install.bat
@@ -0,0 +1,77 @@
+@echo off
+setlocal enableextensions
+
+set me=%~n0
+set parent=%~dp0
+set tmp=%parent%tmp
+
+set /p is_32=Enter "32" for 32 bit, "64" for 64 bit version:
+set lib_version=lib64
+set include_version=include64
+if %is_32%==32  (
+    set lib_version=lib
+    set include_version=include
+    echo %me%: Copying 32 bit version
+) else (
+    echo %me%: Copying 64 bit version
+)
+
+set /p is_lib=Enter "lib" for lib, "bin" for bin version:
+if %is_lib%==bin (
+    set lib_version=bin64
+    if %is_32%==32 (
+        set lib_version=bin
+    )
+    echo %me%: Copying bin version
+) else (
+    echo %me%: Copying lib version
+)
+
+echo %me%: Parent directory: "%parent%"
+echo %me%: Temp directory: "%tmp%"
+
+if exist "%tmp%" rmdir "%tmp%" /q /s
+mkdir "%tmp%"
+
+set ssl_final_lib_path="%parent%..\lib\msw"
+if %is_lib%==bin (
+    set ssl_final_lib_path="%parent%..\bin\msw"    
+)
+if exist %ssl_final_lib_path% rmdir %ssl_final_lib_path% /q /s
+mkdir "%ssl_final_lib_path%\Release"
+mkdir "%ssl_final_lib_path%\Debug"
+echo Final lib path: %ssl_final_lib_path%
+
+set ssl_final_include_path="%parent%..\include\msw"
+if exist %ssl_final_include_path% rmdir %ssl_final_include_path% /q /s
+mkdir %ssl_final_include_path%
+echo Final include path: %ssl_final_include_path%
+
+set version_name=openssl-1.0.2j-vs2015
+echo "%me%: Downloading %version_name% ..."
+set compressed=%tmp%\%version_name%.7z
+start /wait bitsadmin /transfer OpenSSLDownload /download /priority normal http://www.npcglib.org/~stathis/downloads/%version_name%.7z %compressed%
+
+echo "%me%: Decompressing ..."
+set PATH=%PATH%;C:\Program Files\7-Zip\
+7z x %compressed% -o%tmp%
+
+echo "%me%: Copying ..."
+cd %tmp%\%version_name%\%lib_version%\
+
+:: STATIC LIBS
+for %%I in (ssleay32MT.lib libeay32MT.lib appMT.pdb libMT.pdb) do xcopy /F %%I "%ssl_final_lib_path%\Release\"
+for %%I in (ssleay32MTd.lib libeay32MTd.lib appMTd.pdb libMTd.pdb) do xcopy /F %%I "%ssl_final_lib_path%\Debug\"
+
+:: DYNAMIC LIBS
+for %%I in (ssleay32MD.lib ssleay32MD.exp libeay32MD.lib libeay32MD.exp appMD.pdb libMD.pdb) do xcopy /F %%I "%ssl_final_lib_path%\Release\"
+for %%I in (ssleay32MDd.lib ssleay32MDd.exp libeay32MDd.lib libeay32MDd.exp appMDd.pdb libMDd.pdb) do xcopy /F %%I "%ssl_final_lib_path%\Debug\"
+
+xcopy /s /e "%tmp%\%version_name%\%include_version%" %ssl_final_include_path%
+
+echo "%me%: Cleanup..."
+cd %parent%
+rmdir "%tmp%" /q /s
+echo "%me%: DONE."
+
+PAUSE
--- install/install.sh
+++ install/install.sh
@@ -1,30 +1,129 @@
 #!/bin/bash
 
 # Much of the osx and ios build path based on https://gist.github.com/foozmeat/5154962
-
-cd ..
-
-lib_path=""
-target=""
-ios_sdk_version=""
-declare -a config_settings=("debug" "release")
-declare -a config_paths=("/Debug" "/Release")
-
 lower_case=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-echo $lower_case
+ 
+if [ -z $1 ]; then 
+	echo Need to provide platform. Possible platforms are linux, macosx, ios. Exiting!
+	exit 
+fi
+
+#########################
+## create prefix dirs
+#########################
+
+OPENSSL_BASE_DIR=`pwd`/..
+
+PREFIX_BASE_DIR=${OPENSSL_BASE_DIR}/install/tmp
+
+PREFIX_OPENSSL=${PREFIX_BASE_DIR}/openssl_install
+rm -rf ${PREFIX_OPENSSL}
+mkdir -p ${PREFIX_OPENSSL}
+
+#########################
+## create final path
+#########################
+
+FINAL_PATH=`pwd`/..
+LIB_DIR=lib
+INCLUDE_DIR=include
+
+FINAL_LIB_PATH=${FINAL_PATH}/${LIB_DIR}/${lower_case}
+rm -rf ${FINAL_LIB_PATH}
+mkdir -p ${FINAL_LIB_PATH}
+ 
+FINAL_INCLUDE_PATH=${FINAL_PATH}/${INCLUDE_DIR}/${lower_case}
+rm -rf ${FINAL_INCLUDE_PATH}
+mkdir -p ${FINAL_INCLUDE_PATH}
+
+#########################
+## different archs
+#########################
+
+buildOpenSSL()
+{
+	cd ${FINAL_PATH}/openssl
+
+	PREFIX=${PREFIX_OPENSSL}
+	target=$1
+
+	./Configure ${target} --prefix=${PREFIX}
+
+	if [ ${lower_case} = "ios" ]; then
+		sed -id "s!^CFLAG=!CFLAG=-isysroot ${CROSS_TOP}/SDKs/${CROSS_SDK} -miphoneos-version-min=${SDK_VERSION} !" "Makefile"
+	fi 
+
+	make -j 8
+	make install_sw
+	make clean
+
+	if [ -f "${OPENSSL_BASE_DIR}/openssl/Makefiled" ] ; then 
+    rm "${OPENSSL_BASE_DIR}/openssl/Makefiled" 
+  fi
+
+	cp -r ${PREFIX}/include/ ${FINAL_INCLUDE_PATH}
+	cp ${PREFIX}/lib/*.a ${FINAL_LIB_PATH}
+}
+
+buildKey()
+{
+	# key generation code found here...http://stackoverflow.com/questions/6452756/exception-running-boost-asio-ssl-example
+	
+	test_key_path=${OPENSSL_BASE_DIR}/test/SSL/assets/dummy_key
+	temp_bin_path=${PREFIX_OPENSSL}/bin
+	cnf_path=${OPENSSL_BASE_DIR}/openssl/apps/openssl.cnf
+
+	rm -rf ${test_key_path}
+	mkdir -p ${test_key_path}
+	echo "Generating dummy key"
+	${temp_bin_path}/openssl genrsa -out ${test_key_path}/dummy.com.key 2048
+	echo "Generating dummy certificate"
+	${temp_bin_path}/openssl req -new -sha256 -key ${test_key_path}/dummy.com.key -out ${test_key_path}/dummy.com.csr -config ${cnf_path} <<EOF
+us
+cinder
+cinder
+cinder
+cinder
+cinder
+cinder@cinder.com
+cinder
+cinder
+EOF
+	echo "Signing certificate"
+	${temp_bin_path}/openssl x509 -req -days 3650 -in ${test_key_path}/dummy.com.csr -signkey ${test_key_path}/dummy.com.key -out ${test_key_path}/dummy.com.crt 
+	cp ${test_key_path}/dummy.com.key ${test_key_path}/dummy.com.key.secure
+	${temp_bin_path}/openssl rsa -in ${test_key_path}/dummy.com.key.secure -out ${test_key_path}/dummy.com.key -config ${cnf_path}
+	${temp_bin_path}/openssl dhparam -out ${test_key_path}/dh1024.pem 1024
+}
+
 if [ "${lower_case}" = "mac" ] || [ "${lower_case}" = "macosx" ];
 then
-	lib_path="lib/macosx"
-	target="darwin64-x86_64-cc"
+	
+	buildOpenSSL "darwin64-x86_64-cc"
+	buildKey
+
 elif [ "${lower_case}" = "linux" ];
 then
-	lib_path="lib/linux"
-	target="linux-x86_64"
+	
+	buildOpenSSL "linux-x86_64"
+	buildKey
+
 elif [ "${lower_case}" = "ios" ];
 then
-	lib_path="lib/ios"
-	target="iphoneos-cross"
-	ios_sdk_version="9.3"
+	
+	export SDK_VERSION="9.3"
+
+	developer=`xcode-select -print-path`
+	platform="iPhoneOS"
+
+	export $platform
+	export CROSS_TOP="${developer}/Platforms/${platform}.platform/Developer"
+	export CROSS_SDK="${platform}${SDK_VERSION}.sdk"
+	export BUILD_TOOLS="${developer}"
+	export CC="${BUILD_TOOLS}/usr/bin/gcc -arch arm64"
+
+	buildOpenSSL "iphoneos-cross"
+
 else
 	echo "Unkown selection: ${1}"
 	echo "usage: ./install.sh [platform]"
@@ -32,105 +131,5 @@ else
 	exit 1
 fi
 
-build()
-{
-	prefix=$1
-
-	if [ "${config}" = "debug" ]; then
-		./Configure ${target} --${config} --prefix=${prefix}
-	else
-		./Configure ${target} --prefix=${prefix}
-	fi
-
-	make -j 6
-	make install_sw
-	make clean
-}
-
-buildIos()
-{
-	developer=`xcode-select -print-path`
-	platform="iPhoneOS"
-	prefix=$1
-
-	export $platform
-	export CROSS_TOP="${developer}/Platforms/${platform}.platform/Developer"
-	export CROSS_SDK="${platform}${ios_sdk_version}.sdk"
-	export BUILD_TOOLS="${developer}"
-	export CC="${BUILD_TOOLS}/usr/bin/gcc -arch arm64"
-
-	echo "Building openssl for ${platform} ${ios_sdk_version} arm64"
-	if [ "${config}" = "debug" ]; then
-		./Configure ${target} --${config} --prefix=${prefix}
-	else
-		./Configure ${target} --prefix=${prefix}
-	fi
-
-	#sed -id "s!^CFLAG=!CFLAG=isysroot ${CROSS_TOP}/SDKs/${CROSS_SDK} -miphoneos-version-min=${SDK_VERSION} !" "Makefile"
-
-	make -j 6
-	make install_sw
-	make clean
-
-	if [ -f Makefiled ] ; then rm Makefile ; fi
-}
-
-for i in 0 1 
-do
-	config=${config_settings[i]}
-	config_path=${config_paths[i]}
-	# we should be starting in the blocks absolute path
-	block_absolute_path=`pwd`
-	final_library_path=${block_absolute_path}/${lib_path}${config_path}
-	final_include_path=${block_absolute_path}/lib/include
-	temp_library_path=${block_absolute_path}/tmp/openssl
-	
-	echo "Building openssl for ${lower_case}, temp_path: ${temp_library_path}, final_path: ${final_library_path}"
-	
-	rm -rf ${final_library_path}
-	rm -rf ${temp_library_path}
-	rm -rf ${final_include_path}
-	mkdir -p ${final_library_path}
-	mkdir -p ${temp_library_path}
-	mkdir -p ${final_include_path}
-	
-	cd openssl
-	
-	if [ "${lower_case}" = "ios" ]; then		
-		buildIos ${temp_library_path}
-	else
-		build ${temp_library_path}
-	fi
-
-	cd ..
-
-	cp -r ${temp_library_path}/include/ ${final_include_path}
-	cp ${temp_library_path}/lib/*.a ${final_library_path}
-done
-
-# generate a key so that we can test with the app.
-if [ "${lower_case}" != "ios" ]; then
-	# key generation code found here...http://stackoverflow.com/questions/6452756/exception-running-boost-asio-ssl-example
-	current_dir=`pwd`
-	test_key_path=${current_dir}/test/SSL/assets/dummy_key
-	temp_bin_path=${current_dir}/tmp/openssl/bin
-	cnf_path=${current_dir}/openssl/apps/openssl.cnf
-
-	rm -rf ${test_key_path}
-	mkdir -p ${test_key_path}
-	echo "Generating dummy key"
-	${temp_bin_path}/openssl genrsa -out ${test_key_path}/dummy.com.key 2048
-	echo "Generating dummy certificate"
-	${temp_bin_path}/openssl req -new -sha256 -key ${test_key_path}/dummy.com.key -out ${test_key_path}/dummy.com.csr -config ${cnf_path}
-	echo "Signing certificate"
-	${temp_bin_path}/openssl x509 -req -days 3650 -in ${test_key_path}/dummy.com.csr -signkey ${test_key_path}/dummy.com.key -out ${test_key_path}/dummy.com.crt
-	cp ${test_key_path}/dummy.com.key ${test_key_path}/dummy.com.key.secure
-	${temp_bin_path}/openssl rsa -in ${test_key_path}/dummy.com.key.secure -out ${test_key_path}/dummy.com.key -config ${cnf_path}
-	${temp_bin_path}/openssl dhparam -out ${test_key_path}/dh1024.pem 1024
-else
-	echo "Need to run osx config to generate key."
-fi
-
-rm -rf tmp
 echo "Build Complete!"
 
